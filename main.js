37bc1a6411299c899b8b6386406c72359142d711
juliandimitri <121688288+juliandimitri@users.noreply.github.com>
2023-01-03 18:48:33 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+document.onload = cargando_pagina();
+
+
+function cargando_pagina() {
+    let logo_loading = document.querySelector('.loader-section');
+    logo_loading.classList.add('loaded');
+  }
+  
